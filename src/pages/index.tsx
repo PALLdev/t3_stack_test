433e5b30ae8490cddc70b829dd9b7a519edc881f
PALLdev <pblolara21@gmail.com>
2022-08-29 19:07:09 -0400

    testing trcp mutations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { FormEvent, useRef } from "react";
 import { trpc } from "../utils/trpc";
 
 type QuestionCardProps = {
@@ -23,14 +24,61 @@ const Home: NextPage = () => {
         <h1 className="text-5xl md:text-[5rem] leading-normal font-extrabold text-gray-700">
           <span className="text-purple-300">Polls</span> App
         </h1>
-        <p className="text-2xl text-gray-700">Historial de preguntas:</p>
+        <p className="text-2xl text-gray-700">Historial de preguntas</p>
         <div className="grid gap-3 pt-3 mt-3 text-center md:grid-cols-2 lg:w-2/3">
           {data.map((q) => (
             <QuestionCard key={q.id} question={q.question} />
           ))}
         </div>
+        <div className="pt-6 w-full">
+          <p className="text-2xl text-gray-700 text-center">Nueva pregunta</p>
+          <QuestionForm />
+        </div>
       </main>
     </>
+  );
+};
+
+const QuestionForm = () => {
+  const questionRef = useRef<HTMLInputElement>(null);
+  const client = trpc.useContext();
+
+  const { mutate } = trpc.useMutation("question.create", {
+    onSuccess: () => {
+      client.invalidateQueries("question.get-all");
+    },
+  });
+
+  const submitQuestionHandler = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (!questionRef.current) return;
+    mutate({ question: questionRef.current.value });
+    questionRef.current.value = "";
+  };
+
+  return (
+    <form onSubmit={submitQuestionHandler} className="py-4 w-full">
+      <div className="flex flex-col px-12">
+        <label htmlFor="question" className="text-lg">
+          Tu pregunta:
+        </label>
+        <input
+          ref={questionRef}
+          className="border-2 px-2 py-1 text-lg focus:outline-violet-500"
+          id="question"
+          name="question"
+          type="text"
+        />
+      </div>
+      <div className="container text-center">
+        <button
+          type={"submit"}
+          className="bg-purple-300 text-gray-700 ring-1 ring-purple-300 text-lg px-6 py-1 mt-2 rounded-md shadow-sm hover:opacity-80"
+        >
+          Crear
+        </button>
+      </div>
+    </form>
   );
 };
 
--- src/server/router/question.ts
+++ src/server/router/question.ts
@@ -1,7 +1,21 @@
+import { z } from "zod";
 import { createRouter } from "./context";
 
-export const questionRouter = createRouter().query("get-all", {
-  async resolve({ ctx }) {
-    return await ctx.prisma.pollQuestion.findMany();
-  },
-});
+export const questionRouter = createRouter()
+  .query("get-all", {
+    async resolve({ ctx }) {
+      return await ctx.prisma.pollQuestion.findMany();
+    },
+  })
+  .mutation("create", {
+    input: z.object({
+      question: z.string().min(5).max(500),
+    }),
+    async resolve({ ctx, input }) {
+      return await ctx.prisma.pollQuestion.create({
+        data: {
+          question: input.question,
+        },
+      });
+    },
+  });
