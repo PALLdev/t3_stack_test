3236e1c1651678ed918897ee30a916d6da7bbcbd
PALLdev <pblolara21@gmail.com>
2022-08-29 12:07:45 -0400

    testing prisma

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,23 @@
-import type { NextPage } from "next";
+import type {
+  GetServerSideProps,
+  NextPage,
+  InferGetServerSidePropsType,
+} from "next";
 import Head from "next/head";
 import { trpc } from "../utils/trpc";
 
+import { prisma } from "../server/db/client";
+
 type TechnologyCardProps = {
-  name: string;
-  description: string;
-  documentation: string;
+  id: string;
+  question: string;
+  // description: string;
+  // documentation: string;
 };
 
-const Home: NextPage = () => {
+const Home: NextPage = ({
+  questions,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
 
   return (
@@ -25,26 +34,16 @@ const Home: NextPage = () => {
         </h1>
         <p className="text-2xl text-gray-700">This stack uses:</p>
         <div className="grid gap-3 pt-3 mt-3 text-center md:grid-cols-2 lg:w-2/3">
-          <TechnologyCard
-            name="NextJS"
-            description="The React framework for production"
-            documentation="https://nextjs.org/"
-          />
-          <TechnologyCard
-            name="TypeScript"
-            description="Strongly typed programming language that builds on JavaScript, giving you better tooling at any scale"
-            documentation="https://www.typescriptlang.org/"
-          />
-          <TechnologyCard
-            name="TailwindCSS"
-            description="Rapidly build modern websites without ever leaving your HTML"
-            documentation="https://tailwindcss.com/"
-          />
-          <TechnologyCard
-            name="tRPC"
-            description="End-to-end typesafe APIs made easy"
-            documentation="https://trpc.io/"
-          />
+          {!questions ? <div>No questions</div> : <div>{questions}</div>}
+          {/* {questions.map((quest: any) => (
+            <TechnologyCard
+              key={quest.id}
+              id={quest.id}
+              question={quest.question}
+              // description="The React framework for production"
+              // documentation="https://nextjs.org/"
+            />
+          ))} */}
         </div>
         <div className="pt-6 text-2xl text-blue-500 flex justify-center items-center w-full">
           {hello.data ? <p>{hello.data.greeting}</p> : <p>Loading..</p>}
@@ -54,15 +53,11 @@ const Home: NextPage = () => {
   );
 };
 
-const TechnologyCard = ({
-  name,
-  description,
-  documentation,
-}: TechnologyCardProps) => {
+const TechnologyCard = ({ question }: TechnologyCardProps) => {
   return (
     <section className="flex flex-col justify-center p-6 duration-500 border-2 border-gray-500 rounded shadow-xl motion-safe:hover:scale-105">
-      <h2 className="text-lg text-gray-700">{name}</h2>
-      <p className="text-sm text-gray-600">{description}</p>
+      <h2 className="text-lg text-gray-700">{question}</h2>
+      {/* <p className="text-sm text-gray-600">{description}</p>
       <a
         className="mt-3 text-sm underline text-violet-500 decoration-dotted underline-offset-2"
         href={documentation}
@@ -70,9 +65,19 @@ const TechnologyCard = ({
         rel="noreferrer"
       >
         Documentation
-      </a>
+      </a> */}
     </section>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const questions = await prisma.pollQuestion.findMany();
+
+  return {
+    props: {
+      questions: JSON.stringify(questions),
+    },
+  };
 };
 
 export default Home;
