4ee99470694014799aff6d190d08431175959c13
PALLdev <pblolara21@gmail.com>
2022-09-12 12:18:17 -0300

    adding cookie to question response

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,17 @@
-// middleware.ts
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { nanoid } from "nanoid";
 
-// This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  //   return NextResponse.redirect(new URL("/about-2", request.url));
-  console.log(request.cookies);
+  if (request.cookies.get("poll-token")) return;
+
+  const cookie = nanoid();
+  const response = NextResponse.next();
+
+  response.cookies.set("poll-token", cookie, { sameSite: "strict" });
+  return response;
 }
 
-// See "Matching Paths" below to learn more
 export const config = {
   matcher: "/question/:path*",
 };
