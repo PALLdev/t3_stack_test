09d621d41cb3171ace5a24d19d60581cbe5708df
PALLdev <pblolara21@gmail.com>
2022-09-27 03:54:58 -0300

    fixed ssr code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -27,9 +27,22 @@ export default withTRPC<AppRouter>({
 
     return {
       headers() {
-        return {
-          cookie: ctx?.req?.headers.cookie,
-        };
+        if (ctx?.req) {
+          // To use SSR properly, you need to forward the client's headers to the server
+          // This is so you can pass through things like cookies when we're server-side rendering
+          // If you're using Node 18, omit the "connection" header
+          const {
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            connection: _connection,
+            ...headers
+          } = ctx.req.headers;
+          return {
+            ...headers,
+            // Optional: inform server that it's an SSR request
+            "x-ssr": "1",
+          };
+        }
+        return {};
       },
       links: [
         loggerLink({
